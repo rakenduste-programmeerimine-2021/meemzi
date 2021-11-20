dbafdea598b07ffac988b3420afdf19e18331989
reimoramman <reimoramman@online.ee>
2021-11-20 17:57:06 +0200

    Veel sipake BE-i

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,2 +1,5 @@
 //https://www.npmjs.com/package/auth-js
 
+const router = require("express").Router();
+const authController = require("")
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-//import Login from "./pages/Login";
-//import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import {Browserrouter, Route, Switch} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <Browserrouter>
+                <Route exect path="login" component={Login}/>
+                </Browserrouter>
             </header>
         </div>
     );
