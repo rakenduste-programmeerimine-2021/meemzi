1c6993af976527d061e58a93af426978ffbd7431
reimoramman <reimoramman@online.ee>
2021-11-20 23:37:17 +0200

    Sth broken cant see FE. Big sad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,9 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <Browserrouter>
-                <Route exect path="login" component={Login}/>
+                    <Switch>
+                        <Route exect path="login" component={Login}/>
+                    </Switch>
                 </Browserrouter>
             </header>
         </div>
