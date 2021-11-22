f46afb077b8347306d7cef4cd56b3582df9637f3
reimoramman <reimoramman@online.ee>
2021-11-22 19:41:18 +0200

    FE Login ja Rgister saatmine

--- frontend-react/src/components/LoginForm.js
+++ frontend-react/src/components/LoginForm.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './Components.css'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Components.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function LoginForm(){
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (value)=>{
+        value.preventDefault()
+
+        const user ={
+            userName: userName,
+            password: password,
+        }
+
+        try{
+            axios.post('http://localhost:8081/api/auth/signup', user)
+            .then(res =>{
+                console.log(res)
+                //console.log(res,data)
+                if(res.ok){
+                    console.log("Login successful")
+                }
+            }).catch(e=>{
+                console.log(e)
+            })
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+
     return(
         <div className="OuterContainer">
 
--- frontend-react/src/components/RegisterForm.js
+++ frontend-react/src/components/RegisterForm.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Components.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function RegisterForm(){
+    //https://www.tabnine.com/code/javascript/functions/react/setUsername
+    //handle submit at form.js of the link above
+    //https://www.digitalocean.com/community/tutorials/react-axios-react
+
+    const {userName, setUserName} = useState('')
+    const {password, setPassword} = useState('')
+    const {email, setEmail} = useState('')
+    const handleSubmit = (value)=>{
+        value.preventDefault()
+
+        const user ={
+            userName: userName,
+            password: password,
+            email: email
+        }
+
+        try{
+            axios.post('http://localhost:8081/api/auth/signup', user)
+            .then(res =>{
+                console.log(res)
+                //console.log(res,data)
+                if(res.ok){
+                    console.log("New account made")
+                }
+            }).catch(e=>{
+                console.log(e)
+            })
+        }catch(e){
+            console.log(e)
+        }
+
+        //ikka ei saa aru, miks try kogueage finally expectib.
+        //Nvm try ja kui ei t22ta tee hoopis seda :D
+
+    }
+
+    //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#attr-required
+
     return(
         <body>
         <div className="OuterContainer">
 
-            <form className="InnerContainer">
+            <form className="InnerContainer" onSubmit={handleSubmit}>
             <div className="TitleBackround">
                 <h1 className="Titel">Register</h1>
             </div>
@@ -16,6 +56,8 @@ function RegisterForm(){
                 <input
                     type="Text"
                     placeholder="Username"
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
                 ></input>
             </div>
 
@@ -23,6 +65,8 @@ function RegisterForm(){
                 <input
                     type="password"
                     placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                 ></input>
             </div>
 
@@ -30,6 +74,7 @@ function RegisterForm(){
                 <input
                     type="password"
                     placeholder="Confirm Password"
+                    required
                 ></input>
             </div>
 
@@ -37,11 +82,12 @@ function RegisterForm(){
                 <input
                     type="email"
                     placeholder="email"
+                    required
                 ></input>
             </div>
             
             <div className="Register">
-                <button classname="register">Register</button>
+                <button classname="Register">Register</button>
             </div>
 
             <div className="ToLogin">
